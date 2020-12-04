d3d10184f16380a641427a6156ba60c74abb10a4
Matthew Courtney <matthewdc182@gmail.com>
2020-12-03 21:03:15 -0500

    added in the pause function

--- script.js
+++ script.js
@@ -21,21 +21,122 @@ function startTimer() {
         minutesDisplay.textContent = Math.floor(totalSeconds / 60);
         secondsDisplay.textContent = totalSeconds - Math.floor(totalSeconds / 60) * 60
 
-        if(totalSeconds === 0) {
+        if (totalSeconds === 0) {
             clearInterval(timerInterval);
             alert("time is up")
         }
-        stopButton.addEventListener("click", function() {
-            clearInterval(timerInterval);
-            console.log(totalSeconds)
-            totalSeconds = workMinutesInput.value * 60;
-            minutesDisplay.textContent = Math.floor(totalSeconds / 60);
-            secondsDisplay.textContent = "00"
-        })
+
+
+
     }, 1000);
 
+    // Stops the timer and resets the time back to users input
+    stopButton.addEventListener("click", function () {
+        clearInterval(timerInterval);
+        console.log(totalSeconds)
+        totalSeconds = workMinutesInput.value * 60;
+        minutesDisplay.textContent = Math.floor(totalSeconds / 60);
+        secondsDisplay.textContent = "00"
+    });
 
-    
+    // pause the timer starts new interval for rest min and creates a new timer at the end
+    pauseButton.addEventListener("click", function () {
+        console.log("paused");
+        var restMilliseconds = restMinutesInput.value * 60000;
+        console.log(restMilliseconds)
+        clearInterval(timerInterval);
+        console.log(totalSeconds);
+        var restSeconds = restMinutesInput.value * 60;
+        restTimer()
+
+        // rest period timer
+        function restTimer() {
+            var restInterval = setInterval(function () {
+                restSeconds--;
+                minutesDisplay.textContent = Math.floor(restSeconds / 60);
+                secondsDisplay.textContent = restSeconds - Math.floor(restSeconds / 60) * 60
+
+                if (restSeconds === 0) {
+                    clearInterval(restInterval);
+                    function newTimer() {
+
+                        var newInterval = setInterval(function () {
+                            totalSeconds--;
+                            minutesDisplay.textContent = Math.floor(totalSeconds / 60);
+                            secondsDisplay.textContent = totalSeconds - Math.floor(totalSeconds / 60) * 60
+
+                            if (totalSeconds === 0) {
+                                clearInterval(newInterval);
+                                alert("time is up")
+                            }
+
+
+
+                        }, 1000);
+                    }
+
+                }
+
+            }, 1000)
+
+        };
+
+    })
+
+    // pauses the timer
+    //     pauseButton.addEventListener("click", function () {
+    //         console.log(totalSeconds)
+
+    //         var paused = true;
+    //         restMilliseconds = restMinutesInput.value * 60000;
+    //         console.log(restMilliseconds)
+
+    //         setTimeout(restTimer, restMilliseconds);
+
+    //         function restTimer() {
+    //             restSeconds = restMinutesInput.value * 60;
+    //             var restInterval = setInterval(function () {
+    //                 restSeconds--;
+
+    //                 minutesDisplay.textContent = ""
+    //                 secondsDisplay.textContent = ""
+    //                 minutesDisplay.textContent = Math.floor(restSeconds / 60);
+    //                 secondsDisplay.textContent = restSeconds - Math.floor(restSeconds / 60) * 60
+
+    //                 if (restSeconds === 0) {
+    //                     clearInterval(restInterval);
+    //                     alert("rest time is up")
+    //                 }
+
+    //                 playButton.addEventListener("click", function () {
+    //                     if(paused === true) {
+    //                         paused = false;
+    //                         var newTimer = setInterval(function () {
+    //                             totalSeconds--;
+    //                             minutesDisplay.textContent = Math.floor(totalSeconds / 60);
+    //                             secondsDisplay.textContent = totalSeconds - Math.floor(totalSeconds / 60) * 60
+
+    //                             if (totalSeconds === 0) {
+    //                                 clearInterval(newTimer);
+    //                                 alert("time is up")
+    //                             }
+
+
+
+    //                         }, 1000)
+    //                     }
+    //                 })
+
+    //             }, 1000);
+    //             minutesDisplay.textContent = Math.floor(restSeconds / 60);
+    //             secondsDisplay.textContent = restSeconds - Math.floor(restSeconds / 60) * 60
+    //         }
+
+
+
+
+    //     })
+
 }
 
 
